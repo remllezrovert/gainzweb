e497047e37138409d05e714bef52b0f7b3d7d7df
remllezrovert <remllezrovert@gmail.com>
2024-10-01 11:43:33 -0500

    first commit

---
+++ src/services/auth.service.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+//const API_URL = "http://localhost:3000/api/auth/";
+const API_URL = "http://localhost:8081/auth/";
+
+const register = (username, email, password) => {
+  return axios.post(API_URL + "signup", {
+    username,
+    email,
+    password,
+  });
+};
+
+const login = (email, password) => {
+  return axios
+    .post(API_URL + "login", {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.username) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+  return axios.post(API_URL + "signout").then((response) => {
+    return response.data;
+  });
+};
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
+
+const AuthService = {
+  register,
+  login,
+  logout,
+  getCurrentUser,
+}
+
+export default AuthService;
