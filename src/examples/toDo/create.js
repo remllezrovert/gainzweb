7f14ea8dee7d053c25153fd1c280b58ea3bb5d5e
remllezrovert <remllezrovert@gmail.com>
2024-10-11 11:57:13 -0500

    updated examples

--- src/examples/toDo/create.js
+++ src/examples/toDo/create.js
@@ -1,56 +1,56 @@
 
 
 
-const toDoItemInput = document.getElementById("toDoItemInput");
-const toDoItemsDiv = document.getElementById("toDoItems");
+const setItemInput = document.getElementById("setItemInput");
+const setItemsDiv = document.getElementById("setItems");
 
-let toDoItems = [];
-const storageKey = "toDoItems"
-function toDoLoadItems(){
+let setItems = [];
+const storageKey = "setItems"
+function setLoadItems(){
     const oldItems = localStorage.getItem(storageKey);
-    if (oldItems) toDoItems=JSON.parse(oldItems);
-    toDoRenderItems();
+    if (oldItems) setItems=JSON.parse(oldItems);
+    setRenderItems();
 
 }
-function toDoRenderItems() {
-    toDoItemsDiv.innerHTML= null;
-    for (const [idx, toDoItem] of Object.entries(toDoItems)){
+function setRenderItems() {
+    setItemsDiv.innerHTML= null;
+    for (const [idx, setItem] of Object.entries(setItems)){
         const container = document.createElement("div")
         container.style.marginBottom = "10px"
         const text = document.createElement("p") 
         text.style.display = "inline"
         text.style.marginLeft= "10px"
-        text.textContent = toDoItem;
+        text.textContent = setItem;
         const button = document.createElement("button")
         button.textContent = "Delete"
-        button.onclick = () => toDoRemoveItem(idx);
+        button.onclick = () => setRemoveItem(idx);
         container.appendChild(button)
         container.appendChild(text)
-        toDoItemsDiv.appendChild(container)
+        setItemsDiv.appendChild(container)
     }
 }
 
-function toDoSaveItems(){
-    const stringItems = JSON.stringify(toDoItems);
+function setSaveItems(){
+    const stringItems = JSON.stringify(setItems);
     localStorage.setItem(storageKey, stringItems);
 }
 
-function toDoAddItem(){
-    const value = toDoItemInput.value;
+function setAddItem(){
+    const value = setItemInput.value;
     if (!value){
         alert("you cannot add an empty item")
         return
     }
-    toDoItems.push(value)
-    toDoRenderItems()
-    toDoItemInput.value = ""
-    toDoSaveItems()
+    setItems.push(value)
+    setRenderItems()
+    setItemInput.value = ""
+    setSaveItems()
 }
 
 
-function toDoRemoveItem(idx){
-    toDoItems.splice(idx,1)
-    toDoRenderItems()
-    toDoSaveItems()
+function setRemoveItem(idx){
+    setItems.splice(idx,1)
+    setRenderItems()
+    setSaveItems()
 }
-document.addEventListener("DOMContentLoaded", toDoLoadItems)
+document.addEventListener("DOMContentLoaded", setLoadItems)
